e9bfbaa2ed75f2081bb58b2ce82392f587acf3e8
Shay Mualem <Shaymu01@gmail.com>
2017-12-05 12:46:56 +0200

    random stars generation

--- app/src/js/components/stars.jsx
+++ app/src/js/components/stars.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Stars = (props)=>{
+const Stars = (props) => {
+    const numOfStars = 1 + Math.floor(Math.random() * 9);
+
     return (
         <div className="col-5">
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
+            {Array.from(Array(numOfStars)).map(i =>
+                <i key={i} className="fa fa-star"></i>)}
         </div>
     );
 };
