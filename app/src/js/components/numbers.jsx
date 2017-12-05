7179b4116b8b4a3033f40591db9e2fdc9dea3617
Shay Mualem <Shaymu01@gmail.com>
2017-12-05 12:51:34 +0200

    generate the numbers options to select

--- app/src/js/components/numbers.jsx
+++ app/src/js/components/numbers.jsx
@@ -4,12 +4,13 @@ const Numbers = (props) => {
     return (
         <div className="card text-center">
             <div>
-                <span className="number-display">1</span>
-                <span className="number-display selected">2</span>
-                <span className="number-display used">3</span>
+                {Numbers.numbersList.map(i =>
+                    <span key={i} className="number-display">{i}</span>)}
             </div>
         </div>
     );
 };
+
+Numbers.numbersList = Array.from(Array(9), (e, i) => i + 1);
 
 export default Numbers;
